1b44a460cdfd3e0b7d210e3291ddbcb1d97b42cf
Paul <paul@paul.domain>
2020-12-06 13:21:13 +0200

    first commit

---
+++ config.def.h
@@ -0,0 +1,29 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+	"FontAwesome:size=12"
+};
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#d5f1f2", "#87a8af" },
+	[SchemeSel] = { "#ffffff", "#87a8af" },
+	[SchemeSelHighlight] = { "#ffaa66", "#87a8af" },
+	[SchemeNormHighlight] = { "#ffaa66", "#87a8af" },
+	[SchemeOut] = { "#e2f1f6", "#151515" },
+};
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 0;
+/* -h option; minimum height of a menu line */
+static unsigned int lineheight = 20;
+static unsigned int min_lineheight = 8;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
